20dbcc51469db1e4deadd294143f3a927d649870
Chintal <46044461+chintalamintabsbi@users.noreply.github.com>
2018-12-27 12:11:25 +0530

    add city,state,country

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -8,6 +8,9 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CityComponent } from './city/city.component';
+import { StateComponent } from './state/state.component';
+import { CountryComponent } from './country/country.component';
 
 
 
@@ -19,6 +22,6 @@ import { HeaderComponent } from './components/header/header.component';
         TranslateModule,
         NgbDropdownModule
     ],
-    declarations: [LayoutComponent, SidebarComponent, HeaderComponent]
+    declarations: [LayoutComponent, SidebarComponent, HeaderComponent, CityComponent, StateComponent, CountryComponent]
 })
 export class LayoutModule {}
